931d1748bdbfdf3d61628fefc2444ab58522c04f
lauris <lauris@ma-1.lv>
2013-10-30 19:14:18 +0200

    bunp version

--- example/example.js
+++ example/example.js
@@ -1,6 +1,9 @@
 var watchdir = require('../');
 
-
-watchdir(__dirname + '/monitor', function (file) {
+var unwatch = watchdir(__dirname + '/monitor', function (file) {
   console.log(file);
 });
+
+setTimeout(function () {
+  unwatch();
+}, 5 + 60 * 1000);  // 5 min
